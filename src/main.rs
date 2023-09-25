8449277bdc3992e3a580906899cf63a997d71c05
lthoerner <lowellashton@gmail.com>
2023-09-25 13:44:58 -0700

    fix(handler): separate SHIFT and NONE handlers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::io::{stdout, Stdout};
 
 use crossterm::cursor;
-use crossterm::event;
-use crossterm::style;
+use crossterm::event::{self, Event, KeyCode, KeyModifiers};
+use crossterm::style::Print;
 use crossterm::terminal;
 use crossterm::{execute, queue};
 
@@ -23,6 +23,13 @@ impl LineBuffer {
         // TODO: Add bounds check? Maybe unnecessary
         self.buffer.insert(self.cursor_index, c);
         self.right();
+    }
+
+    fn insert_str(&mut self, s: &str) {
+        self.buffer.insert_str(self.cursor_index, s);
+        for _ in 0..s.chars().count() {
+            self.right();
+        }
     }
 
     fn left(&mut self) {
@@ -60,8 +67,6 @@ fn main() {
 }
 
 fn prompt(prefix: &str) -> String {
-    terminal::enable_raw_mode().unwrap();
-
     let mut line_buffer = LineBuffer::default();
     let mut ctx = Context {
         stdout: stdout(),
@@ -69,61 +74,71 @@ fn prompt(prefix: &str) -> String {
         prompt_width: prefix.chars().count(),
     };
 
-    execute!(ctx.stdout, style::Print(prefix)).unwrap();
+    terminal::enable_raw_mode().unwrap();
+    execute!(ctx.stdout, Print(prefix)).unwrap();
     loop {
         if handle(&mut ctx, &mut line_buffer, event::read().unwrap()) {
             terminal::disable_raw_mode().unwrap();
-            execute!(ctx.stdout, style::Print("\n")).unwrap();
+            execute!(ctx.stdout, Print("\n")).unwrap();
             return line_buffer.buffer;
         }
     }
 }
 
-fn handle(ctx: &mut Context, line: &mut LineBuffer, event: event::Event) -> bool {
+fn handle(ctx: &mut Context, line: &mut LineBuffer, event: Event) -> bool {
     match event {
-        event::Event::Key(key_event) => {
-            if key_event.modifiers == event::KeyModifiers::NONE
-            // $ Need to split SHIFT off from NONE
-                || key_event.modifiers == event::KeyModifiers::SHIFT
-            {
+        Event::Key(key_event) => {
+            if key_event.modifiers == KeyModifiers::NONE {
                 match key_event.code {
-                    event::KeyCode::Char(c) => {
+                    KeyCode::Char(c) => {
                         line.insert(c);
                         redraw_buffer(ctx, line);
                     }
-                    event::KeyCode::Backspace => {
+                    KeyCode::Backspace => {
                         line.backspace();
                         redraw_buffer(ctx, line);
                     }
-                    event::KeyCode::Delete => {
+                    KeyCode::Delete => {
                         line.delete();
                         redraw_buffer(ctx, line);
                     }
-                    event::KeyCode::Left => {
+                    KeyCode::Left => {
                         line.left();
                         update_cursor(ctx, line);
                     }
-                    event::KeyCode::Right => {
+                    KeyCode::Right => {
                         line.right();
                         update_cursor(ctx, line);
                     }
-                    event::KeyCode::Enter => {
+                    KeyCode::Enter => {
                         return true;
                     }
-                    _ => exit(1, "UNHANDLED KEY"),
+                    _ => exit(1, "UNSUPPORTED KEY"),
+                }
+            } else if key_event.modifiers == KeyModifiers::SHIFT {
+                match key_event.code {
+                    KeyCode::Char(c) => {
+                        line.insert(c);
+                        redraw_buffer(ctx, line);
+                    }
+                    KeyCode::Right => {
+                        line.insert_str("DEBUG ");
+                        redraw_buffer(ctx, line);
+                    }
+                    _ => exit(1, "UNSUPPORTED KEY COMBINATION"),
                 }
             } else {
                 #[allow(clippy::match_single_binding)]
                 match (key_event.modifiers, key_event.code) {
-                    _ => exit(1, "NO MODIFIERS SUPPORTED YET"),
+                    _ => exit(1, "UNSUPPORTED KEY COMBINATION"),
                 }
             }
         }
-        event::Event::Mouse(_) => exit(1, "MOUSE CAPTURE SHOULD BE DISABLED"),
-        event::Event::Resize(_, _) => todo!(), // Need to reflow the text most likely
-        event::Event::FocusGained => (),
-        event::Event::FocusLost => (),
-        event::Event::Paste(_) => exit(1, "BRACKETED PASTE SHOULD BE DISABLED"),
+        Event::Mouse(_) => exit(1, "MOUSE CAPTURE SHOULD BE DISABLED"),
+        Event::Resize(_, _) => todo!(), // Need to reflow the text most likely
+        Event::FocusGained => (),
+        Event::FocusLost => (),
+        Event::Paste(_) => exit(1, "BRACKETED PASTE SHOULD BE DISABLED"),
     }
 
     false
@@ -131,26 +146,32 @@ fn handle(ctx: &mut Context, line: &mut LineBuffer, event: event::Event) -> bool
 
 fn redraw_buffer(ctx: &mut Context, line: &LineBuffer) {
     update_cursor(ctx, line);
-    execute!(ctx.stdout, cursor::SavePosition,).unwrap();
+    execute!(ctx.stdout, cursor::SavePosition).unwrap();
     queue!(
         ctx.stdout,
         terminal::Clear(terminal::ClearType::FromCursorDown),
         // $ This will cause a bug if the prompt is wider than the terminal, need to divide
         cursor::MoveTo(ctx.prompt_width as u16, ctx.y_origin),
-        style::Print(&line.buffer)
+        Print(&line.buffer)
     )
     .unwrap();
     execute!(ctx.stdout, cursor::RestorePosition).unwrap();
 }
 
 fn update_cursor(ctx: &mut Context, line: &LineBuffer) {
+    let terminal_height = terminal::size().unwrap().1;
     let (x, y) = cursor_index_to_coord(ctx, line.cursor_index);
+    // If the cursor must overrun the end of the terminal window, the terminal must be scrolled down
+    if y > terminal_height {
+        // TODO: Viewport calculations and scrolling
+    }
+
     execute!(ctx.stdout, cursor::MoveTo(x, y)).unwrap();
 }
 
 // $ This will probably cause a bug when the terminal is resized or when the user scrolls
 fn cursor_index_to_coord(ctx: &mut Context, cursor_index: usize) -> (u16, u16) {
-    let (terminal_width, _) = terminal::size().unwrap();
+    let terminal_width = terminal::size().unwrap().0;
     let true_position = ctx.prompt_width + cursor_index;
 
     let x = (true_position % terminal_width as usize) as u16;
